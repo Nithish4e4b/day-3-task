b894061fe17e18d552e3fb4d88e07b478150b622
Nithish Kumar N <127470114+Nithish4e4b@users.noreply.github.com>
2023-03-16 19:54:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let myResume={
+    "basics": {
+        "id":{
+        "name": "NITHISH KUMAR N",
+        "email": "nithishkumar.17@hotmail.com",
+        "phone": 9489325128,
+        },
+      "location": {
+        "address": "13/53, V.O.C Nagar, 2nd Street",
+        "postalCode": 628501,
+        "city": "Kovilpatti",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "linkedin": "https://www.linkedin.com/in/nithish-kumar-nk/",
+          "github":"https://github.com/Nithish4e4b"
+        }
+      ]
+    },
+    "certificatons": [
+        {
+          "name": "Certified Ethical Hacker(CEH V11)",
+          "id": "ECC0241576893",
+          "issueDate": "25-08-2022",
+          "expiryDate": "24-08-2025",
+        },
+      ],
+
+    "skills": [
+      {
+        "name": "C,Python,Javascript,linux,NMAP,NETCAT,Wireshark,Burpsuit,OSINT,OWASP Top 10,Knowledge about Embedded devices",
+        "level": "intermediate",
+        "project": [
+          "Finger Print Voting System Using RaspberryPI"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Coding , Playing games, Playing CTF, watching Youtube",
+      }
+    ]
+  }
+  console.log(myResume);
